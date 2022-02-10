d7bc85c103de67502b9c2a7aaab96f41cb6a070d
Ernest-Gray <egray4cs@gmail.com>
2022-02-09 23:54:25 -0500

    General Update

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.css']
+  styleUrls: ['./education.component.css'],
 })
 export class EducationComponent implements OnInit {
+  array = [
+    {
+      name: 'Harford Community College',
+      description:
+        'I achieved my 2 year degree in both computer science and information technology',
+    },
+    {
+      name: 'Towson University',
+      description:
+        'Where I currently am enrolled.  I am an undergraduate studying computer science and plan to graduate in the spring of 2023',
+    },
+    {
+      name: 'High School',
+      description:
+        'I was homeschooled my whole pre-college life.  My mother taught me and in my mid-teens I started to learn through online courses.  Durring this entire time I participated in numerous co-operative education opportunities which allowed me to socialize with lots of other homeschoolers.',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
