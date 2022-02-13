da0dbb3bae961d07d246416bccdf1c71931b2236
Ernest-Gray <egray4cs@gmail.com>
2022-02-12 22:01:07 -0500

    update for nick

--- src/app/portfolio-list/portfolio-list.component.ts
+++ src/app/portfolio-list/portfolio-list.component.ts
@@ -7,16 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio-list.component.css'],
 })
 export class PortfolioListComponent {
-
   content = [
-    { fieldName: 'VR', content: [{ name: 'VR-Extreme-Bowling', imageRef: "/assets/Ultamite Bowling Home.PNG" }] },
-    { fieldName: 'Front-End', content: [{ name: 'This Website', imageRef: "", link: "" }] },
-    { fieldName: 'Misc', content: [{name: 'World of Warcraft Autonamous Fishing', imageRef: "/assets/WoW Fishing Icon.png", link: "" }] },
+    {
+      fieldName: 'VR',
+      content: [
+        {
+          name: 'VR-Extreme-Bowling',
+          imageRef: '/assets/Ultamite Bowling Home.PNG',
+          link: '',
+          description: 'VR Bowling',
+        },
+        {
+          name: 'VR-Extreme-Bowling',
+          imageRef: '/assets/Ultamite Bowling Home.PNG',
+          link: '',
+          description: 'VR Bowling',
+        },
+        {
+          name: 'VR-Extreme-Bowling',
+          imageRef: '/assets/Ultamite Bowling Home.PNG',
+          link: '',
+          description: 'VR Bowling',
+        },
+        {
+          name: 'VR-Extreme-Bowling',
+          imageRef: '/assets/Ultamite Bowling Home.PNG',
+          link: '',
+          description: 'VR Bowling',
+        },
+      ],
+    },
+    {
+      fieldName: 'Front-End',
+      content: [
+        {
+          name: 'This Website',
+          imageRef: '',
+          link: '',
+          description: 'I created this website',
+        },
+      ],
+    },
+    {
+      fieldName: 'Misc',
+      content: [
+        {
+          name: 'World of Warcraft Autonamous Fishing',
+          imageRef: '/assets/WoW Fishing Icon.png',
+          link: '',
+          description: 'A Fishing Bot for the popular game World of Warcraft',
+        },
+      ],
+    },
     // { fieldName: 'test', content: [{name: 'test1', imageRef: "" }, {name: 'test2', imageRef: "" }, {name: 'test3', imageRef: "" }] },
   ];
 
-  constructor(public GetIsMobileService: GetIsMobileService) {
-  }
+  constructor(public GetIsMobileService: GetIsMobileService) {}
 
   viewIndex = 0;
   changeView(index: any) {
